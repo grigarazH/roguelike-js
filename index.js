715ffa83bea8d8bd395d41eb8d7555b20e8db1c5
grigarazH <45718270+grigarazH@users.noreply.github.com>
2022-11-02 18:13:42 +0300

    feat: added player and movement

--- index.js
+++ index.js
@@ -4,14 +4,26 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
+function Hero() {
+    this.maxHP = 20;
+    this.hp = 20;
+    this.swordLevel = 0;
+    this.minAtk = [1, 3, 6];
+    this.maxAtk = [3, 6, 9];
+    this.x = 0;
+    this.y = 0;
+}
+
+
 function Game() {
     this.field = [];
+    this.player = new Hero();
 }
 
 Game.prototype.init = function() {
-    for(var i = 0; i < 13; i++) {
+    for(var i = 0; i < 26; i++) {
         var row = [];
-        for(var j = 0; j < 20; j++) {
+        for(var j = 0; j < 40; j++) {
             row[j] = 'W';
         }
         this.field[i] = row;
@@ -29,13 +41,24 @@ Game.prototype.init = function() {
     for(var i = 0; i < vCorridorCount; i++) {
         this.addCorridor('V');
     }
+    var freeSpaces = [];
+    for(var i = 0; i < 26; i++) {
+        for(var j = 0; j < 40; j++) {
+            if(this.field[i][j] === 'F') freeSpaces.push([i, j]);
+        }
+    }
+
+    var playerPosI = getRandomInt(0, freeSpaces.length);
+    this.player.x = freeSpaces[playerPosI][1];
+    this.player.y = freeSpaces[playerPosI][0];
+    freeSpaces.splice(playerPosI, 1);
 }
 
 Game.prototype.addRoom = function() {
     var width = getRandomInt(3, 9);
     var height = getRandomInt(3, 9);
-    var startX = getRandomInt(0, 20 - width - 1);
-    var startY = getRandomInt(0, 13 - height - 1);
+    var startX = getRandomInt(0, 40 - width - 1);
+    var startY = getRandomInt(0, 26 - height - 1);
 
     for(var i = startY; i < startY + height; i++) {
         for(var j = startX; j < startX + width; j++) {
@@ -46,13 +69,13 @@ Game.prototype.addRoom = function() {
 
 Game.prototype.addCorridor = function(direction) {
     if(direction === 'H') {
-        var y = getRandomInt(0, 13);
-        for (var j = 0; j < 20; j++) {
+        var y = getRandomInt(0, 26);
+        for (var j = 0; j < 40; j++) {
             this.field[y][j] = 'F';
         }
     }else {
-        var x = getRandomInt(0, 19);
-        for (var i = 0; i < 13; i++) {
+        var x = getRandomInt(0, 40);
+        for (var i = 0; i < 26; i++) {
             this.field[i][x] = 'F';
         }
     }
@@ -61,8 +84,8 @@ Game.prototype.addCorridor = function(direction) {
 Game.prototype.render = function() {
     var fieldEl = document.querySelector(".field");
     fieldEl.innerHTML = "";
-    for(var i = 0; i< 13; i++) {
-        for(var j = 0; j< 20; j++) {
+    for(var i = 0; i< 26; i++) {
+        for(var j = 0; j< 40; j++) {
             var tile = document.createElement("div");
             tile.classList.add("tile");
             switch(this.field[i][j]) {
@@ -72,9 +95,42 @@ Game.prototype.render = function() {
                 default:
                     break;
             }
-            tile.style.top = 50 * i + "px";
-            tile.style.left = 50 * j + "px";
+            if(this.player.x === j && this.player.y === i) {
+                tile.classList.add("tileP");
+                var playerHpEl = document.createElement("div");
+                playerHpEl.classList.add("health");
+                playerHpEl.style.width = this.player.hp * 5 + "%";
+                tile.appendChild(playerHpEl);
+            }
+            tile.style.top = 25 * i + "px";
+            tile.style.left = 25 * j + "px";
             fieldEl.appendChild(tile);
         }
     }
 }
+
+document.addEventListener("keydown", function (e){
+    switch(e.key.toUpperCase()) {
+        case 'W':
+            if(game.player.y > 0 && game.field[game.player.y - 1][game.player.x] === 'F') {
+                game.player.y -= 1;
+            }
+            break;
+        case 'S':
+            if(game.player.y < 25 && game.field[game.player.y + 1][game.player.x] === 'F') {
+                game.player.y += 1;
+            }
+            break;
+        case 'D':
+            if(game.player.x < 39 && game.field[game.player.y][game.player.x + 1] === 'F') {
+                game.player.x += 1;
+            }
+            break;
+        case 'A':
+            if(game.player.x > 0 && game.field[game.player.y][game.player.x - 1] === 'F') {
+                game.player.x -= 1;
+            }
+            break;
+    }
+    game.render();
+});
